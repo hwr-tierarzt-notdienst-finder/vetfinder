fb99336ef19a429d7a5868025713d0fb39db4d85
OrhunDoenmez <orhun.doenmez@gmail.com>
2022-09-03 12:39:02 +0200

    changed look of vet information

--- frontend/lib/vet_information.dart
+++ frontend/lib/vet_information.dart
@@ -17,11 +17,18 @@ class VetInformation extends StatelessWidget {
     String id = arguments.id;
     Veterinarian vet = getVeterinarianById(id);
 
+    const descTextStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontSize: 18,
+      height: 2,
+    );
     return Scaffold(
       appBar: AppBar(
         title: const Text('Vet Information'),
       ),
-      body: Center(
+      // 2 rows with 8 columns
+      body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
@@ -31,6 +38,13 @@ class VetInformation extends StatelessWidget {
               style: const TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 30,
+              ),
+            ),
+            const Text(
+              'vet.praxisName',
+              style: TextStyle(
+                fontStyle: FontStyle.italic,
+                fontSize: 20,
               ),
             ),
             const SizedBox(height: 10),
@@ -59,15 +73,56 @@ class VetInformation extends StatelessWidget {
             ),
             const SizedBox(height: 10),
             Row(
-              children: [
-                const Icon(Icons.public, size: 20.0),
+              children: const [
                 Text(
-                  vet.websiteUrl,
-                  style: const TextStyle(
-                    fontSize: 15,
-                  ),
+                  'vet.services',
+                  style: descTextStyle,
                 ),
               ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              children: const [
+                Text(
+                  'vet.pets',
+                  style: descTextStyle,
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              children: const [
+                Text(
+                  'vet.mail',
+                  style: descTextStyle,
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              children: const [
+                Text(
+                  'vet.website',
+                  style: descTextStyle,
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: MediaQuery.of(context).size.height * 0.1,
+              margin: const EdgeInsets.only(bottom: 20.0, top: 30.0),
+              child: const Center(
+                child: Text(
+                  'vet.description',
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ),
             Container(
               decoration: BoxDecoration(
