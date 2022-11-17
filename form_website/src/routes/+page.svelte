84bf7cfbe799db1b952c85edb78a299869d8f68b
Denis Knecht <denis.knecht@mail.de>
2022-11-17 19:02:44 +0100

    first draft of homepage and form page

--- form_website/src/routes/+page.svelte
+++ form_website/src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	function redirectForm() {
+		goto('/form');
+	}
+</script>
+
+<div class="hero min-h-full bg-base-100">
+	<div class="hero-content max-w-none min-w-screen">
+		<div class="max-w-xl flex flex-col justify-center items-center">
+			<h1 class="mb-2 text-5xl font-bold text-center">Hallo!</h1>
+			<h1 class="text-2xl text-center">
+				VetFinder ist ein Projekt zur Anzeige von den aktuell geöffneten Tiernotdiensten
+			</h1>
+			<div class="mt-4 stats stats-vertical lg:stats-horizontal shadow bg-primary">
+				<div class="stat">
+					<div class="stat-title">Tierärzte insgesamt</div>
+					<div class="stat-value">1.433</div>
+					<div class="stat-desc">seit November 2022</div>
+				</div>
+
+				<div class="stat">
+					<div class="stat-title">Neue Tierärzte</div>
+					<div class="stat-value">265</div>
+				</div>
+
+				<div class="stat">
+					<div class="stat-title">Nutzer monatlich</div>
+					<div class="stat-value">20.569</div>
+					<div class="stat-desc">↗︎ 1.112 (5%)</div>
+				</div>
+			</div>
+			<h1 class="mt-10 text-xl text-center">
+				Du bist ein Tierarzt? Dann <button class="btn btn-primary btn-sm" on:click={redirectForm}
+					>klicke hier</button
+				>
+			</h1>
+		</div>
+	</div>
+</div>
---
+++ form_website/src/routes/form/+page.svelte
@@ -0,0 +1,112 @@
+<div class="flex min-w-full justify-center">
+	<div class="flex flex-col gap-10 justify-center items-center">
+		<div class="flex flex-col gap-2">
+			<h1 class="text-2xl font-bold">Kontaktdaten</h1>
+			<div class="flex gap-4">
+				<div class="form-control w-full max-w-xs">
+					<label class="label">
+						<span class="label-text">E-Mail</span>
+					</label>
+					<input
+						type="text"
+						placeholder="Hier eingeben"
+						class="input input-bordered w-full max-w-xs"
+					/>
+				</div>
+				<div class="form-control w-full max-w-xs">
+					<label class="label">
+						<span class="label-text">Telefonnummer</span>
+					</label>
+					<input
+						type="text"
+						placeholder="Hier eingeben"
+						class="input input-bordered w-full max-w-xs"
+					/>
+				</div>
+				<div class="form-control w-full max-w-xs">
+					<label class="label">
+						<span class="label-text">Placeholder</span>
+					</label>
+					<input
+						type="text"
+						placeholder="Hier eingeben"
+						class="input input-bordered w-full max-w-xs"
+					/>
+				</div>
+			</div>
+		</div>
+		<div class="divider" />
+		<div class="flex flex-col gap-2">
+			<h1 class="text-2xl font-bold">Vollständiger Name</h1>
+			<div class="flex gap-4">
+				<div class="form-control w-full max-w-xs">
+					<label class="label">
+						<span class="label-text">Titel</span>
+					</label>
+					<input
+						type="text"
+						placeholder="Hier eingeben"
+						class="input input-bordered w-full max-w-xs"
+					/>
+				</div>
+				<div class="form-control w-full max-w-xs">
+					<label class="label">
+						<span class="label-text">Dein Vorname</span>
+					</label>
+					<input
+						type="text"
+						placeholder="Hier eingeben"
+						class="input input-bordered w-full max-w-xs"
+					/>
+				</div>
+				<div class="form-control w-full max-w-xs">
+					<label class="label">
+						<span class="label-text">Dein Nachname</span>
+					</label>
+					<input
+						type="text"
+						placeholder="Hier eingeben"
+						class="input input-bordered w-full max-w-xs"
+					/>
+				</div>
+			</div>
+		</div>
+		<div class="divider" />
+		<div class="flex flex-col gap-2">
+			<h1 class="text-2xl font-bold">Adresse</h1>
+			<div class="flex gap-4">
+				<div class="form-control w-full max-w-xs">
+					<label class="label">
+						<span class="label-text">Straße</span>
+					</label>
+					<input
+						type="text"
+						placeholder="Hier eingeben"
+						class="input input-bordered w-full max-w-xs"
+					/>
+				</div>
+				<div class="form-control w-full max-w-xs">
+					<label class="label">
+						<span class="label-text">Hausnummer</span>
+					</label>
+					<input
+						type="text"
+						placeholder="Hier eingeben"
+						class="input input-bordered w-full max-w-xs"
+					/>
+				</div>
+				<div class="form-control w-full max-w-xs">
+					<label class="label">
+						<span class="label-text">Postleitzahl</span>
+					</label>
+					<input
+						type="text"
+						placeholder="Hier eingeben"
+						class="input input-bordered w-full max-w-xs"
+					/>
+				</div>
+			</div>
+		</div>
+		<button class="btn btn-primary btn-lg">Eintragen</button>
+	</div>
+</div>
