5c30f92f7ab59d78cf13fc3c2f37180d48a156a1
0scarB <oscarb@protonmail.com>
2022-11-22 15:27:22 +0100

    Use port 80 in production

--- backend/src/entrypoints/app.sh
+++ backend/src/entrypoints/app.sh
@@ -4,8 +4,17 @@ ENTRYPOINTS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && p
 
 cd "$ENTRYPOINTS_DIR/.." || exit 1
 
-if [ "$ENV" = 'prod' ]; then
-    uvicorn app:app
+# Load environment variables from .env files
+. ../.env
+if [ -f ../.env.secret ]; then
+    . ../.env.secret
+fi
+if [ -f ../.env.local ]; then
+    . ../.env.local
+fi
+
+if [ "$ENV" = 'dev' ]; then
+    uvicorn app:app --port "$(eval echo \$\{FASTAPI_"${ENV^^}"_PORT\})" --reload
 else
-    uvicorn app:app --reload
+    uvicorn app:app --port "$(eval echo \$\{FASTAPI_"${ENV^^}"_PORT\})"
 fi
