f3dfdd614a187e3777c1c1981d371a0d1ab35462
0scarB <oscarb@protonmail.com>
2022-12-06 09:15:43 +0100

    Add treatments endpoint

--- backend/src/api/__init__.py
+++ backend/src/api/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from models import Treatments
 from . import vets
 from . import form
 from . import content_management
@@ -9,3 +10,8 @@ api = FastAPI()
 api.include_router(vets.router)
 api.include_router(form.router)
 api.include_router(content_management.router)
+
+
+@api.get("/treatments")
+def get_treatments() -> list[str]:
+    return sorted(treatment for treatment in Treatments.__members__.values())
