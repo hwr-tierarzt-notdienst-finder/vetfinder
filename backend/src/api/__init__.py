7f3e4702e8d0cff0b5d398da4df3bbf1951606df
Oscar Butler-Aldridge <oscarb@protonmail.com>
2022-12-06 10:47:38 +0100

    Add localhost CORS (#42)

--- backend/src/api/__init__.py
+++ backend/src/api/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from models import Treatments
 from . import vets
@@ -6,7 +7,28 @@ from . import form
 from . import content_management
 
 
+origins = [
+    "http://localhost",
+    "https://localhost",
+    "http://localhost:8000",
+    "http://localhost:5173",
+    "http://127.0.0.1",
+    "https://127.0.0.1",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:5173",
+]
+
 api = FastAPI()
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 api.include_router(vets.router)
 api.include_router(form.router)
 api.include_router(content_management.router)
